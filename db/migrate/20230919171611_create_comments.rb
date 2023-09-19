f5ce924d9f1288191831eb81a21ea762b4d49210
Alexandre <alexsales001@hotmail.com>
2023-09-19 17:07:06 -0300

    Reviewer fixes

--- db/migrate/20230919171611_create_comments.rb
+++ db/migrate/20230919171611_create_comments.rb
@@ -3,7 +3,7 @@
 class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
-      t.references :author, foreign_key: { to_table: :users }
+      t.references :user, foreign_key: true  # changed from :author to :user
       t.references :post, null: false, foreign_key: true
       t.text :text
 
