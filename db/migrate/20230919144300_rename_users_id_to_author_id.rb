6adcb1a0d77c67111b9331ae8af262da72b9c715
Alexandre <alexsales001@hotmail.com>
2023-09-19 11:44:03 -0300

    changes to new file

--- db/migrate/20230919144300_rename_users_id_to_author_id.rb
+++ db/migrate/20230919144300_rename_users_id_to_author_id.rb
@@ -1,4 +1,7 @@
 class RenameUsersIdToAuthorId < ActiveRecord::Migration[7.0]
   def change
+    rename_column :posts, :users_id, :author_id
+    rename_column :comments, :users_id, :author_id
+    rename_column :likes, :users_id, :author_id
   end
 end
