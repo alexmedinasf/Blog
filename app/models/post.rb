fc090bf6144a202e9bf9889349b47781e574cc11
Alexandre <alexsales001@hotmail.com>
2023-09-21 16:29:33 -0300

    method which returns the 5 most recent comments for a given post

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,4 +9,10 @@ class Post < ApplicationRecord
   end
 
   after_save :update_posts_counter
+
+  # method which returns the 5 most recent comments for a given post
+  def recent_comments
+    comments.order(created_at: :desc).limit(5).reverse
+  end
+
 end
