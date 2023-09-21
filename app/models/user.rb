07b0bd55f43648c038adb6c19fdaeb7582139d93
Alexandre <alexsales001@hotmail.com>
2023-09-21 16:31:24 -0300

    method to return the 3 most recent posts for a given user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ApplicationRecord
     has_many :posts, foreign_key: :author_id
     has_many :comments, foreign_key: :author_id
     has_many :likes, foreign_key: :author_id
+
+    # method to return the 3 most recent posts for a given user
+  def recent_posts
+    posts.order(created_at: :desc).limit(3).reverse
+  end
 end
