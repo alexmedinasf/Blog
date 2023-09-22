51b67e2dab5668a151a14e24bc266a8c6b20822c
Alexandre <alexsales001@hotmail.com>
2023-09-22 10:51:21 -0300

    added validations to comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,10 @@ class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
 
+  validates :author, presence: true
+  validates :post, presence: true
+  validates :text, presence: true, length: { maximum: 150 }
+
   # method to update the comments counter for a post
   def update_comments_counter
     post.increment!(:comments_counter)
