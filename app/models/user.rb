e4cbfbaf2d7db7404d8e34add33de436c13f22f7
Alexandre <alexsales001@hotmail.com>
2023-09-22 10:52:26 -0300

    added validations to user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,11 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: :author_id
   has_many :likes, foreign_key: :author_id
 
+  validates :name, presence: true, length: { in: 2..50 }
+  validates :photo, presence: true
+  validates :bio, presence: true, length: { in: 2..500 }
+  validates :posts_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   # method to return the 3 most recent posts for a given user
   def recent_posts
     posts.order(created_at: :desc).limit(3).reverse
