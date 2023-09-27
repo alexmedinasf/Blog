851d680229e02def379f23cd5fafcccf52263ea8
Alexandre <alexsales001@hotmail.com>
2023-09-27 16:51:44 -0300

    remove controllers actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,9 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
-    render :index
+
   end
 
   def show
-    @post = Post.find(params[:id])
-    render :show
+
   end
 end
