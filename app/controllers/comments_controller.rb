0d4215f7ca72a512662d2d541effc20b16bf3eb0
Alexandre <alexsales001@hotmail.com>
2023-09-29 15:22:22 -0300

    comment_controller method

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,22 @@
 class CommentsController < ApplicationController
     def new
+      @post = Post.find(params[:id])
+      @user = User.find(params[:user_id])
+      @comment = Comment.new
     end
   
     def create
+      @post = Post.find(params[:post_id])
+      @user = User.find(params[:user_id])
+      comment = Comment.new(params.require(:comment).permit(:text))
+      comment.author = current_user
+      comment.post = @post
+      if comment.save
+        flash[:success] = "Comment created successfully"
+        redirect_to user_post_path(@user.id, @post.id)
+      else
+        flash.now[:error] = "Error: Comment could not be created"
+        render :new, locals: { comment: @comment }
+      end
     end
   end
