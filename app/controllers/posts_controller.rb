a4eda3f8cd2b69a039d790ca2e05ca0e2086dd75
Alexandre <alexsales001@hotmail.com>
2023-09-29 15:51:41 -0300

    rubocop

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,9 @@
 class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
-    @posts = @user.posts
+    @posts = @user.posts.order(created_at: :desc)
   end
+
   def show
     @post = Post.find(params[:id])
     @user = User.find(@post.author_id)
@@ -11,7 +12,7 @@ class PostsController < ApplicationController
   def new
     post = Post.new
     respond_to do |f|
-      f.html { render :new, locals: { post: post } }      
+      f.html { render :new, locals: { post: } }
     end
   end
 
@@ -21,13 +22,19 @@ class PostsController < ApplicationController
     respond_to do |f|
       f.html do
         if post.save
-          flash[:success] = "Post created successfully"
+          flash[:success] = 'Post created successfully'
           redirect_to user_posts_path(current_user.id)
         else
-          flash.now[:error] = "Error: Post could not be created"
-          render :new, locals: { post: post }
+          flash.now[:error] = 'Error: Post could not be created'
+          render :new, locals: { post: }
         end
       end
     end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end
