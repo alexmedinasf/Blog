17d7049833bfb5e50c43c78d5e1d7ada428bcd0d
Alexandre <alexsales001@hotmail.com>
2023-09-29 15:18:29 -0300

    new posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,9 +10,24 @@ class PostsController < ApplicationController
 
   def new
     post = Post.new
+    respond_to do |f|
+      f.html { render :new, locals: { post: post } }      
+    end
   end
 
   def create
-
+    post = Post.new(params.require(:post).permit(:title, :text))
+    post.author = current_user
+    respond_to do |f|
+      f.html do
+        if post.save
+          flash[:success] = "Post created successfully"
+          redirect_to user_posts_path(current_user.id)
+        else
+          flash.now[:error] = "Error: Post could not be created"
+          render :new, locals: { post: post }
+        end
+      end
+    end
   end
 end
