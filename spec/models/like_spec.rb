adc05ac522138eb3697feac924c137aaca2c897c
Alexandre <alexsales001@hotmail.com>
2023-10-02 18:56:52 -0300

    fix post, like spec for model

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,23 +1,19 @@
 require 'rails_helper'
-
 RSpec.describe Like, type: :model do
   before(:each) do
     @user = User.new(name: 'Jack Sparrow', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
                      bio: 'I\'m Captain Jack Sparrow!', posts_counter: 2)
     @post = Post.new(author: @user, title: 'The Black Pearl', text: 'What a ship..!',
-                     comments_counter: 2, likes_counter: 4)
+                     comments_counter: 2, likes_counter: 0)
+    @like = Like.create(author: @user, post: @post)
   end
 
-  subject { Like.new(author: @user, post: @post) }
-
-  before { subject.save }
-
   it 'should be a like instance' do
-    expect(subject).to be_an_instance_of(Like)
+    expect(@like).to be_an_instance_of(Like)
   end
 
   it 'should test the likes_counter method' do
-    expect(@post.likes_counter).to eq(5)
+    expect(@post.likes_counter).to eq(1)
   end
 
   # Tests for associations and connections
@@ -26,21 +22,19 @@ RSpec.describe Like, type: :model do
       like = Like.reflect_on_association(:author)
       expect(like.macro).to eq(:belongs_to)
     end
-
     it 'should belong to an post' do
       like = Like.reflect_on_association(:post)
       expect(like.macro).to eq(:belongs_to)
     end
   end
-
   # Tests for the attributes of like
   describe 'the attributes of a like' do
     it 'should have an author' do
-      expect(subject.author).to eq(@user)
+      expect(@like.author).to eq(@user)
     end
 
     it 'should have a post' do
-      expect(subject.post).to eq(@post)
+      expect(@like.post).to eq(@post)
     end
   end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,20 +1,14 @@
 require 'rails_helper'
-
 RSpec.describe Post, type: :model do
   before(:each) do
     @user = User.new(name: 'Jack Sparrow', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
                      bio: 'I\'m Captain Jack Sparrow!', posts_counter: 0)
+    @post = Post.create(author: @user, title: 'The Black Pearl', text: 'What a ship, it was...',
+                        comments_counter: 0, likes_counter: 0)
   end
-
-  subject do
-    Post.new(author: @user, title: 'The Black Pearl', text: 'What a ship, it was...',
-             comments_counter: 0, likes_counter: 0)
-  end
-
-  before { subject.save }
 
   it 'should be a post instance' do
-    expect(subject).to be_an_instance_of(Post)
+    expect(@post).to be_an_instance_of(Post)
   end
 
   it 'should test the posts_counter method' do
@@ -22,53 +16,43 @@ RSpec.describe Post, type: :model do
   end
 
   it 'should test the recent_comments method' do
-    expect(subject.recent_comments).to eq([])
+    expect(@post.recent_comments).to eq([])
   end
 
   # Tests for validations
   it 'is valid with valid attributes' do
-    expect(subject).to be_valid
+    expect(@post).to be_valid
   end
 
   describe 'the proper form of a title' do
     it 'is not valid without a title' do
-      subject.title = nil
-      expect(subject).to_not be_valid
+      @post.title = nil
+      expect(@post).to_not be_valid
     end
     it 'is not valid with a title longer than 250 characters' do
-      subject.title = 'a' * 251
-      expect(subject).to_not be_valid
+      @post.title = 'a' * 251
+      expect(@post).to_not be_valid
     end
   end
 
   describe 'the proper form of a text' do
     it 'is not valid without a text' do
-      subject.text = nil
-      expect(subject).to_not be_valid
+      @post.text = nil
+      expect(@post).to_not be_valid
     end
     it 'is not valid with a text longer than 500 characters' do
-      subject.text = 'a' * 501
-      expect(subject).to_not be_valid
+      @post.text = 'a' * 501
+      expect(@post).to_not be_valid
     end
   end
 
   it 'is not valid without an author' do
-    case subject.author
+    case @post.author
     when nil
-      expect(subject).to_not be_valid
+      expect(@post).to_not be_valid
     when @user
-      expect(subject).to be_valid
+      expect(@post).to be_valid
     end
-  end
-
-  it 'must have a likes_counter' do
-    subject.likes_counter = nil
-    expect(subject).to_not be_valid
-  end
-
-  it 'must have a comments_counter' do
-    subject.comments_counter = nil
-    expect(subject).to_not be_valid
   end
 
   # Tests for associations
@@ -77,12 +61,10 @@ RSpec.describe Post, type: :model do
       post = Post.reflect_on_association(:author)
       expect(post.macro).to eq(:belongs_to)
     end
-
     it 'should possibly have many comments' do
       post = Post.reflect_on_association(:comments)
       expect(post.macro).to eq(:has_many)
     end
-
     it 'should possibly have many likes' do
       post = Post.reflect_on_association(:likes)
       expect(post.macro).to eq(:has_many)
