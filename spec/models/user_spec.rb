6ce8895f6a081f9a24a0649f5003321eb20b8dc4
Alexandre <alexsales001@hotmail.com>
2023-10-02 18:59:53 -0300

    fix user and comment specs for model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,23 +1,17 @@
 require 'rails_helper'
-
 RSpec.describe User, type: :model do
   subject { User.new(name: 'Jack Sparrow', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'You will always remember this as the day that you almost caught Captain Jack Sparrow!', posts_counter: 0) }
-
   before { subject.save }
-
   it 'should be a user instance' do
     expect(subject).to be_an_instance_of(User)
   end
-
   it 'should test the recent_posts method' do
     expect(subject.recent_posts).to eq([])
   end
-
   # Tests for validations
   it 'is valid with valid attributes' do
     expect(subject).to be_valid
   end
-
   describe 'the proper form of a name' do
     it 'is not valid without a name' do
       subject.name = nil
@@ -32,7 +26,6 @@ RSpec.describe User, type: :model do
       expect(subject).to_not be_valid
     end
   end
-
   describe 'the proper form of a bio' do
     it 'is not valid without a bio' do
       subject.bio = nil
@@ -47,14 +40,8 @@ RSpec.describe User, type: :model do
       expect(subject).to_not be_valid
     end
   end
-
   it 'is not valid without a photo' do
     subject.photo = nil
-    expect(subject).to_not be_valid
-  end
-
-  it 'must have a posts_counter' do
-    subject.posts_counter = nil
     expect(subject).to_not be_valid
   end
 
@@ -64,12 +51,10 @@ RSpec.describe User, type: :model do
       u = User.reflect_on_association(:comments)
       expect(u.macro).to eq(:has_many)
     end
-
     it 'should possibly have many likes' do
       u = User.reflect_on_association(:likes)
       expect(u.macro).to eq(:has_many)
     end
-
     it 'should possibly have many posts' do
       u = User.reflect_on_association(:posts)
       expect(u.macro).to eq(:has_many)
