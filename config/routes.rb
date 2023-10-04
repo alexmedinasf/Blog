f4e0d2a503f6d19278a612df1648005279028591
Alexandre <alexsales001@hotmail.com>
2023-10-04 18:21:53 -0300

    added user model and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root 'users#index'
+  devise_for :users, controllers: { sessions: 'users/sessions',
+                                    registrations: 'users/registrations',
+                                    passwords: 'users/passwords' }
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ('/')
+  root 'home#index'
+
+  # Route to list all users
+  # Route to show a specific user
   resources :users, only: [:index, :show] do
+    # Route to show all posts of a specific user
+    # Route to show a specific post of a specific user
     resources :posts, only: [:index, :new, :create, :show] do
       resources :comments, only: [:new, :create]
       resources :likes, only: [:create]
