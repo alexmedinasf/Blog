049c7eae62c4102e234727c07910e12e5b1f1178
dani <danifromecuador@gmail.com>
2023-10-05 13:10:25 -0500

    Add password controller

--- app/controllers/users/confirmations/passwords_controller.rb
+++ app/controllers/users/confirmations/passwords_controller.rb
@@ -1,34 +1,30 @@
-# frozen_string_literal: true
-
-class Users::Confirmations::PasswordsController < Devise::PasswordsController
+class Users::PasswordsController < Devise::PasswordsController
   # GET /resource/password/new
   # def new
   #   super
   # end
-
   # POST /resource/password
   # def create
   #   super
   # end
-
   # GET /resource/password/edit?reset_password_token=abcdef
   # def edit
   #   super
   # end
-
   # PUT /resource/password
   # def update
   #   super
   # end
 
-  # protected
+  protected
 
   # def after_resetting_password_path_for(resource)
   #   super(resource)
   # end
 
   # The path used after sending reset password instructions
-  # def after_sending_reset_password_instructions_path_for(resource_name)
-  #   super(resource_name)
-  # end
+  def after_sending_reset_password_instructions_path_for(resource_name)
+    flash[:notice] = 'Please check your terminal for the reset password instructions.'
+    new_session_path(resource_name)
+  end
 end
