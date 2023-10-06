3400e281ca149001647f5aeb4696d1e8baf81d56
dani <danifromecuador@gmail.com>
2023-10-06 17:58:41 -0500

    Create users controller for th API

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,14 @@
+class Api::V1::UsersController < ApiController
+  def index
+    @users = User.all.order(created_at: :desc)
+
+    render json: @users, status: :ok 
+  end
+
+  def show
+    @user = User.find(params[:id])
+
+    render json: @user, status: :ok
+  end
+
+end
