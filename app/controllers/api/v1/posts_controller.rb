b67ea8fa34c9cd7eb7026c98b95272ab33632872
Alexandre <alexsales001@hotmail.com>
2023-10-06 19:10:44 -0300

    add comment and post controller for api endpoint

---
+++ app/controllers/api/v1/posts_controller.rb
@@ -0,0 +1,9 @@
+class Api::V1::PostsController < ApiController
+    before_action :authorize_request
+    
+    def index
+      @user = User.find(params[:user_id])
+      @posts = @user.posts.order(created_at: :desc)
+      render json: @posts
+    end
+  end
