dd02721226a35cff298649a4012895bddca3dca7
Alexandre <alexsales001@hotmail.com>
2023-10-06 15:18:01 -0300

    update: user, delete button  and methods

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,30 +1,35 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
-  has_many :comments
-  has_many :likes
-
+  has_many :comments, dependent: :destroy, foreign_key: :post_id
+  has_many :likes, dependent: :destroy, foreign_key: :post_id
   validates :title, presence: true, length: { maximum: 250 }
   validates :text, presence: true, length: { maximum: 500 }
   validates :author, presence: true, length: { maximum: 50 }
   validates :likes_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :comments_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
-  # method to update the posts counter for a user
-  def update_posts_counter
-    author.increment!(:posts_counter)
+  # A method that updates the posts counter for a user.
+
+  def self.update_posts_counter(user_id)
+    user = User.find(user_id)
+    counter = user.posts.count
+    user.update(posts_counter: counter)
   end
 
-  after_save :update_posts_counter
+  #def update_posts_counter
+    # author.increment!(:posts_counter)
+   # author.posts_counter = author.posts.count
+  #end
 
-  # method which returns the 5 most recent comments for a given post
+  #after_save :self.update_posts_counter
+  #after_destroy :self.update_posts_counter
+
+  # A method which returns the 5 most recent comments for a given post.
   def recent_comments
     comments.includes(:author).order(created_at: :desc).limit(5).reverse
   end
-
   before_validation :set_defaults
-
   private
-
   def set_defaults
     self.likes_counter = 0
     self.comments_counter = 0
