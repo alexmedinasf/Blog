bbde50e81b0bddd9e9d34e2266e144b6e51abd2d
Alexandre <alexsales001@hotmail.com>
2023-10-07 10:56:03 -0300

    minor changes and implemented sign-up api functionality

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,8 +1,9 @@
 class Api::V1::CommentsController < ApiController
+  before_action :authorize_request
+
   def index
     @post = Post.find(params[:post_id])
     @comments = @post.comments.order(created_at: :desc)
-
     render json: @comments
   end
 
@@ -10,7 +11,6 @@ class Api::V1::CommentsController < ApiController
     @comment = Comment.new(text: params[:comment][:text])
     @comment.author = @current_user
     @comment.post_id = params[:post_id]
-
     if @comment.save
       render json: @comment, status: :created
     else
@@ -18,9 +18,7 @@ class Api::V1::CommentsController < ApiController
               status: :unprocessable_entity
     end
   end
-
   private
-
   def comment_params
     params.require(:comment).permit(:text)
   end
